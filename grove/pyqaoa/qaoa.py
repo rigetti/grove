68a90760523b4de45f508929887d241408b3cbdf
Nick Rubin <rubinnc0@gmail.com>
2017-01-12 01:41:24 -0800

    Adapting to new pyquil wavefunction API

--- grove/pyqaoa/qaoa.py
+++ grove/pyqaoa/qaoa.py
@@ -213,7 +213,7 @@ class QAOA(object):
 
         param_prog = self.get_parameterized_program()
         prog = param_prog(angles)
-        wf = self.qvm.wavefunction(prog)
+        wf, _ = self.qvm.wavefunction(prog)
         wf = wf.reshape((-1, 1))
         probs = np.zeros_like(wf)
         for xx in xrange(2 ** self.n_qubits):
