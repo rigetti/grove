00dc0095642d8684927eebc4bc6d110020541337
Joseph Lin <joelin2014@gmail.com>
2017-06-21 09:31:29 -0700

    Remove blackbox_algorithms from this branch and clean up stand alone bv code.

--- grove/bernstein_vazirani/bernstein_vazirani.py
+++ grove/bernstein_vazirani/bernstein_vazirani.py
@@ -8,12 +8,12 @@ def oracle_function(vec_a, b, qubits, ancilla):
     """
     Defines an oracle that performs the following unitary transformation:
     |x>|y> -> |x>|f(x) xor y>
-    :param unitary_funct: Matrix representation of the function f, i.e. the
-                          unitary transformation that must be applied to a
-                          state |x> to put f(x) in qubit 0.
-    :param qubits: List of qubits that enter as input |x>.
-    :param ancilla: Qubit to serve as input |y>.
-    :param scratch_bit: Empty qubit to be used as scratch space.
+
+    f(x) is given by a*x+b, where * is a bitwise dot product and everything is taken mod 2.
+    :param vec_a: a vector of length n containing only ones and zeros.
+    :param b: a 0 or 1
+    :param qubits: List of qubits that enter as input |x>. Must be the same length (n) as vec_a
+    :param ancilla: Qubit to serve as input |y>, where the answer will be written to.
     :return: A program that performs the above unitary transformation.
     :rtype: Program
     """
@@ -50,25 +50,24 @@ def bernstein_vazirani(oracle, qubits, ancilla):
     p.inst(map(H, qubits))
     return p
 
-def bitstring_to_array(bitstring):
-    return np.array(map(int, bitstring))
-
 if __name__ == "__main__":
     import pyquil.forest as forest
 
+    # ask user to input the value for a
     bitstring = raw_input("Give a bitstring representation for the vector a: ")
     while not (all([num in ('0', '1') for num in bitstring])):
         print "The bitstring must be a string of ones and zeros."
         bitstring = raw_input("Give a bitstring representation for the vector a: ")
-    vec_a = bitstring_to_array(bitstring)
+    vec_a = np.array(map(int, bitstring))
 
-
+    # ask user to input the value for b
     b = int(raw_input("Give a single bit for b: "))
     while b not in {0, 1}:
         print "b must be either 0 or 1"
         b = int(raw_input("Give a single bit for b: "))
 
 
+    # First, create the program to find a
     bv_program = pq.Program()
     qubits = [bv_program.alloc() for _ in range(len(vec_a))]
     ancilla = bv_program.alloc()
@@ -77,12 +76,16 @@ if __name__ == "__main__":
     bv_program += bernstein_vazirani(oracle, qubits, ancilla)
     bv_program.out()
 
-    print bv_program
     qvm = forest.Connection()
     results = qvm.run_and_measure(bv_program, [q.index() for q in qubits])
     print "The bitstring a is given by: ", "".join(map(str, results[0][::-1]))
 
+    # Reset the qubits to all 0s and feed through to get b
     bv_program.inst(RESET)
     bv_program += oracle
     results = qvm.run_and_measure(bv_program, [ancilla.index()])
+
     print "b is given by: ", results[0][0]
+
+    print "Full program given by: "
+    print bv_program
