97e559f3cd20119cded1ebcf7b8d116ce6b19e90
Aaron Vontell <vontell@mit.edu>
2017-06-20 22:17:37 -0700

    Fix dependency spec error

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     url="https://github.com/rigetticomputing/grove.git",
     download_url="https://github.com/rigetticomputing/grove/tarball/1.0.0",
     packages=["grove", "grove.pyqaoa", "grove.pyvqe", "grove.qft",
-              "grove.phaseestimation", "grove.teleport", "grove.grover"],
+              "grove.phaseestimation", "grove.teleport", "grove.amplification"],
     install_requires=[
         'numpy',
         'scipy',
