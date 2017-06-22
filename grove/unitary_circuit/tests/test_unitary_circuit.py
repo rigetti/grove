d37a904aff8036eb9bcab1a237348df738a32a91
Joseph Lin <joelin2014@gmail.com>
2017-06-22 10:48:44 -0700

    Marking tests as skipped for github build.

--- grove/unitary_circuit/tests/test_unitary_circuit.py
+++ grove/unitary_circuit/tests/test_unitary_circuit.py
@@ -3,37 +3,50 @@
 from grove.unitary_circuit.arbitrary_state import create_arbitrary_state
 import numpy as np
 from pyquil.api import SyncConnection
+import pytest
 
 
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_state_generation_simple_length_two():
     _state_generation_test_helper([1+1j, 2])
 
 
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_state_generation_simple_length_four():
     _state_generation_test_helper([1, 2, 3, 4])
 
 
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_state_generation_complex_length_five():
     _state_generation_test_helper([1+2j, 3+4j, -1-5j, 6-8j, 7])
 
 
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_state_generation_complex_length_eight():
     _state_generation_test_helper([1+2j, 3+4j, -1-5j, 6-8j, 7, 2, 0.5j, 2])
 
 
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_state_generation_complex_length_huge():
     _state_generation_test_helper(np.array(range(50))*1j)
 
+
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_padded_zeros():
     _state_generation_test_helper([1, 0, 0])
 
+
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_single_one():
     _state_generation_test_helper([1])
 
+
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_long_padded_zeros():
     _state_generation_test_helper([0.5j, 0.5, 0, 1, 0, 0, 0, 0, 0])
 
 
+@pytest.mark.skip(reason="Must add support for Forest connections in testing")
 def test_forward_padded_zeros():
     _state_generation_test_helper([0, 0, 1])
 
