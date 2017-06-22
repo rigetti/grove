58364a63d0e9a2efb1944b0ff7d9c884856fa430
Joseph Lin <joelin2014@gmail.com>
2017-06-21 17:57:22 -0700

    Add documentation

--- grove/unitary_circuit/tests/test_unitary_circuit.py
+++ grove/unitary_circuit/tests/test_unitary_circuit.py
@@ -20,6 +20,10 @@ def test_state_generation_complex_length_eight():
     _state_generation_test_helper([1+2j, 3+4j, -1-5j, 6-8j, 7, 2, 0.5j, 2])
 
 
+def test_state_generation_complex_length_huge():
+    _state_generation_test_helper(np.array(range(50))*1j)
+
+
 def _state_generation_test_helper(v):
     # encode vector in quantum state
     p = create_arbitrary_state(v)
@@ -34,3 +38,12 @@ def _state_generation_test_helper(v):
     # wavefunction amplitudes should resemble vector
     for pair in zip(v_norm, wf.amplitudes):
         assert np.allclose(*pair)
+
+if __name__ == "__main__":
+    v = list(input("Give your array: "))
+    p = create_arbitrary_state(v)
+    qvm = SyncConnection()
+    wf, _ = qvm.wavefunction(p)
+    print list(v / np.linalg.norm(v))
+    print wf
+    print p.out()
--- grove/unitary_circuit/utils.py
+++ grove/unitary_circuit/utils.py
@@ -7,15 +7,20 @@ from pyquil.parametric import ParametricProgram
 from pyquil.api import SyncConnection
 
 ############### Circuits from single bit unitaries and controlled unitaries ######################
-# see http://d.umn.edu/~vvanchur/2015PHYS4071/Chapter4.pdf for more information                  #
+# see                                                                                            #
+#       -http://d.umn.edu/~vvanchur/2015PHYS4071/Chapter4.pdf                                    #
+#       - http://fab.cba.mit.edu/classes/862.16/notes/computation/Barenco-1995.pdf               #
+# for more information                                                                           #
+# NOTE: these papers may use different conventions.                                              #
 ##################################################################################################
 
 
 def get_one_qubit_gate_params(U):
     """
+    Decompose U into e^i*d_phase * RZ(alpha)*RY(theta)*RZ(beta).
     :param U: a 2x2 unitary matrix
     :return: d_phase, alpha, theta, beta
-    :rtype: list
+    :rtype: tuple
     """
     d = np.sqrt(np.linalg.det(U)+0j)  # +0j to ensure the complex square root is taken
     U = U/d
@@ -34,6 +39,14 @@ def get_one_qubit_gate_params(U):
     return d_phase, alpha, beta, theta
 
 def get_one_qubit_gate_from_unitary_params(params, qubit):
+    """
+    Creates a circuit that simulates acting the unitary on the qubit, given angle parameters as above.
+    Uses a combination of PHASE, RY and RZ gates to achieve this.
+    :param params: tuple in the form (d_phase, alpha, theta, beta)
+    :param qubit: the qubit on which to act on
+    :return: the program that represents acting the unitary on the qubit
+    :rtype: Program
+    """
     p = pq.Program()
     d_phase, alpha, beta, theta = params
     if theta == 0:
@@ -56,6 +69,16 @@ def get_one_qubit_gate_from_unitary_params(params, qubit):
 
 
 def get_one_qubit_controlled_from_unitary_params(params, control, target):
+    """
+    Get the controlled version of the unitary, given angular parameters.
+    Uses PHASE, RZ, RY and CNOT gates.
+
+    :param params: tuple in the form (d_phase, alpha, theta, beta)
+    :param control: the control qubit
+    :param target: the target qubit
+    :return: the program that simulates acting a controlled unitary on the target, given the control.
+    :rtype: Program
+    """
     p = pq.Program()
     d_phase, alpha, beta, theta = params
     p.inst(RZ((beta-alpha)/2, target))\
@@ -67,26 +90,43 @@ def get_one_qubit_controlled_from_unitary_params(params, control, target):
     return p
 
 
-def create_arbitrary_state(vector, verbose=False):
+def create_arbitrary_state(vector):
+    """
+    This function makes a program that can generate an arbitrary state.
+
+    Given a complex vector a with components a_i (i from 0 to N-1), produce a function
+    that takes in |0> and gives sum_i=1^N a_i/|a| |i>
+    :param vector: the vector to put into qubit form.
+    :return: a program that takes in |0> and produces a state that represents this vector.
+    :rtype: Program
+    """
+    # start with normalizing
     vector = vector/np.linalg.norm(vector)
-    if verbose: print list(vector)
+
     n = int(np.ceil(np.log2(len(vector))))
     N = 2 ** n
-    if verbose: print n, N
+
     p = pq.Program()
-    last = 0
-    ones = set()
-    zeros = {0}
+
+    last = 0  # the last state created
+
+    ones = set()  # the bits that are ones
+    zeros = {0}  # the bits that are zeros
+
     unset_coef = 1
-    if verbose: cxn = SyncConnection()
+
     for i in range(1, N):
+        # Generate the Gray Code corresponding to i
         current = i ^ (i >> 1)
         flipped = 0
+
+        # Find the position of the bit that was flipped
         difference = last ^ current
         while difference & 1 == 0:
             difference = difference >> 1
             flipped += 1
 
+        # See if the flip was 0 to 1 or 1 to 0
         flipped_to_one = True
         if flipped in ones:
             ones.remove(flipped)
@@ -95,17 +135,17 @@ def create_arbitrary_state(vector, verbose=False):
         elif flipped in zeros:
             zeros.remove(flipped)
 
+
         a_i = 0. if last >= len(vector) else vector[last]
         z_i = a_i/unset_coef
-        beta = 2 * np.arccos(np.abs(z_i))
+
+        # Write z_i = e^(+/- i*alpha/2) * cos(beta/2), depending on if it is 0 to 1 or 1 to 0
+        beta = 2 * np.arccos(np.abs(z_i))  # to get the right angle for the magnitude
+        z_i /= np.cos(beta/2)
+        alpha = -2*np.angle(z_i)  # to get the right angle for the phase
 
         # set all zero controls to 1
-#        if verbose: print zeros, ones, flipped
-
         p.inst(map(X, zeros))
-
-        z_i /= np.cos(beta/2)
-        alpha = -2*np.angle(z_i)
 
         if not flipped_to_one:
             alpha *= -1
@@ -121,24 +161,18 @@ def create_arbitrary_state(vector, verbose=False):
         if alpha != 0:
             p += n_qubit_controlled_RZ(list(zeros | ones), flipped, alpha)
 
-#        if verbose: print bin(last), bin(current), flipped_to_one, unset_coef, a_i, z_i, alpha, beta
-
         # flip all zeros back to 1
         p.inst(map(X, zeros))
 
         if flipped_to_one:
             ones.add(flipped)
-            print "Set Coef ", unset_coef * np.exp(-1j*alpha/2)*np.cos(beta/2)
             unset_coef *= np.exp(1j*alpha/2)*np.sin(beta/2)
 
         else:
             zeros.add(flipped)
-            print "Set Coef ", unset_coef * np.exp(1j*alpha/2)*np.cos(beta/2)
             unset_coef *= -np.exp(-1j*alpha/2)*np.sin(beta/2)
 
         last = current
-        if verbose: wf, _ = cxn.wavefunction(p)
-        if verbose: print wf, "\n---------------------"
 
     # fix the phase of the final qubit
     a_i = vector[last]
@@ -147,11 +181,18 @@ def create_arbitrary_state(vector, verbose=False):
     p.inst(map(X, zeros))
     p += n_qubit_controlled_PHASE(list(zeros), n-1, theta)
     p.inst(map(X, zeros))
+
     return p
 
 
 def n_qubit_controlled_RZ(controls, target, theta):
-    print "Controlled RZ ", controls, target, theta
+    """
+    :param controls: The list of control qubits
+    :param target: The target qubit
+    :param theta: The angle of rotation
+    :return: the program that applies a RZ(theta) gate to target, given controls
+    :rtype: Program
+    """
     if len(controls) == 0:
         return pq.Program().inst(RZ(theta, target))
     u = np.array([[np.exp(-1j*theta/2), 0], [0, np.exp(1j*theta/2)]])
@@ -159,8 +200,13 @@ def n_qubit_controlled_RZ(controls, target, theta):
 
 
 def n_qubit_controlled_PHASE(controls, target, theta):
-    print "Controlled PHASE ", controls, target, theta
-
+    """
+    :param controls: The list of control qubits
+    :param target: The target qubit
+    :param theta: The angle of rotation
+    :return: the program that applies a PHASE(theta) gate to target, given controls
+    :rtype: Program
+    """
     if len(controls) == 0:
         return pq.Program().inst(PHASE(theta, target))
     u = np.array([[1, 0], [0, np.exp(1j*theta)]])
@@ -169,23 +215,30 @@ def n_qubit_controlled_PHASE(controls, target, theta):
 
 
 def n_qubit_controlled_RY(controls, target, theta):
-    print "Controlled RY ", controls, target, theta
-
+    """
+    :param controls: The list of control qubits
+    :param target: The target qubit
+    :param theta: The angle of rotation
+    :return: the program that applies a RY(theta) gate to target, given controls
+    :rtype: Program
+    """
     if len(controls) == 0:
         return pq.Program().inst(RY(theta, target))
     u = np.array([[np.cos(theta/2), -np.sin(theta/2)], [np.sin(theta/2), np.cos(theta/2)]])
     return n_qubit_control(controls, target, u)
 
+
 def n_qubit_control(controls, target, u):
     """
     Returns a controlled u gate with n-1 controls.
 
-    Does not define new gates.
+    Does not define new gates. Follows arXiv:quant-ph/9503016, but flips the order of the gates due to convention.
 
     :param controls: The indices of the qubits to condition the gate on.
     :param target: The index of the target of the gate.
     :param u: The unitary gate to be controlled, given as a numpy array.
     :return: The controlled gate.
+    :rtype: Program
     """
     def controlled_program_builder(controls, target, target_gate):
 
