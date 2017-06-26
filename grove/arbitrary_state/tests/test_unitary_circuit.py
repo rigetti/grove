983688084b8dd74f218801615c87e6c0b3317a62
Joseph Lin <joelin2014@gmail.com>
2017-06-26 11:10:04 -0700

    Add tests for uniformlycontrolledrotationmatrix

--- grove/arbitrary_state/tests/test_unitary_circuit.py
+++ grove/arbitrary_state/tests/test_unitary_circuit.py
@@ -1,59 +1,95 @@
 """Tests for utils"""
 
-import numpy as np
 import pytest
-from grove.arbitrary_state.arbitrary_state import create_arbitrary_state
-from pyquil.api import SyncConnection
+
+from grove.arbitrary_state.arbitrary_state import *
 
 
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_state_generation_simple_length_two():
-    _state_generation_test_helper([1+1j, 2])
+class TestCreateArbitraryState(object):
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_state_generation_simple_length_two(self):
+        _state_generation_test_helper([1+1j, 2])
+    
+    
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_state_generation_simple_length_four(self):
+        _state_generation_test_helper([1, 2, 3, 4])
+    
+    
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_state_generation_complex_length_five(self):
+        _state_generation_test_helper([1+2j, 3+4j, -1-5j, 6-8j, 7])
+    
+    
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_state_generation_complex_length_eight(self):
+        _state_generation_test_helper([1+2j, 3+4j, -1-5j, 6-8j, 7, 2, 0.5j, 2])
+    
+    
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_state_generation_complex_length_huge(self):
+        _state_generation_test_helper(np.array(range(50))*1j)
+    
+    
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_padded_zeros(self):
+        _state_generation_test_helper([1, 0, 0])
+    
+    
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_single_one(self):
+        _state_generation_test_helper([1], 4)
+    
+    
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_long_padded_zeros(self):
+        _state_generation_test_helper([0.5j, 0.5, 0, 1, 0, 0, 0, 0, 0], 3)
+    
+    
+    @pytest.mark.skip(reason="Must add support for Forest connections in testing")
+    def test_forward_padded_zeros(self):
+        _state_generation_test_helper([0, 0, 1])
 
 
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_state_generation_simple_length_four():
-    _state_generation_test_helper([1, 2, 3, 4])
+class TestUniformlyControlledRotationMatrix(object):
+    def test_one_control(self):
+        expected = 1/2.*np.array([[1, 1],
+                                  [1, -1]])
+        actual = uniformly_controlled_rotation_matrix(1)
+        assert np.allclose(expected, actual)
 
 
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_state_generation_complex_length_five():
-    _state_generation_test_helper([1+2j, 3+4j, -1-5j, 6-8j, 7])
+    def test_two_controls(self):
+        expected = 1/4.*np.array([[1, 1, 1, 1],
+                                  [1, -1, 1, -1],
+                                  [1, -1, -1, 1],
+                                  [1, 1, -1, -1]])
+        actual = uniformly_controlled_rotation_matrix(2)
+        print expected
+        print actual
+
+        assert np.allclose(expected, actual)
 
 
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_state_generation_complex_length_eight():
-    _state_generation_test_helper([1+2j, 3+4j, -1-5j, 6-8j, 7, 2, 0.5j, 2])
+    def test_three_controls(self):
+        expected = 1 / 8. * np.array([[1, 1, 1, 1, 1, 1, 1, 1],
+                                      [1, -1, 1, -1, 1, -1, 1, -1],
+                                      [1, -1, -1, 1, 1, -1, -1, 1],
+                                      [1, 1, -1, -1, 1, 1, -1, -1],
+                                      [1, 1, -1, -1, -1, -1, 1, 1],
+                                      [1, -1, -1, 1, -1, 1, 1, -1],
+                                      [1, -1, 1, -1, -1, 1, -1, 1],
+                                      [1, 1, 1, 1, -1, -1, -1, -1]])
+        actual = uniformly_controlled_rotation_matrix(3)
+        print expected
+        print actual
 
-
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_state_generation_complex_length_huge():
-    _state_generation_test_helper(np.array(range(50))*1j)
-
-
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_padded_zeros():
-    _state_generation_test_helper([1, 0, 0])
-
-
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_single_one():
-    _state_generation_test_helper([1], 4)
-
-
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_long_padded_zeros():
-    _state_generation_test_helper([0.5j, 0.5, 0, 1, 0, 0, 0, 0, 0], 3)
-
-
-@pytest.mark.skip(reason="Must add support for Forest connections in testing")
-def test_forward_padded_zeros():
-    _state_generation_test_helper([0, 0, 1])
+        assert np.allclose(expected, actual)
 
 
 def _state_generation_test_helper(v, offset=0):
     # encode vector in quantum state
-    p = create_arbitrary_state(v)
+    p = create_arbitrary_state(v, offset)
     qvm = SyncConnection()
     wf, _ = qvm.wavefunction(p)
 
