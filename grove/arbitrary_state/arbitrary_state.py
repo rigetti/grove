473ab72a8f84be64c6af9a8484d4d61bfb1932bd
Joseph Lin <joelin2014@gmail.com>
2017-06-26 10:30:03 -0700

    Add some modularity and additional documentation

--- grove/arbitrary_state/arbitrary_state.py
+++ grove/arbitrary_state/arbitrary_state.py
@@ -1,9 +1,45 @@
 """Class for generating a program that can generate an arbitrary quantum state
 """
-import pyquil.quil as pq
 import numpy as np
-from pyquil.gates import *
+import pyquil.quil as pq
 from pyquil.api import SyncConnection
+from pyquil.gates import *
+
+
+def uniformly_controlled_rotation_matrix(k):
+    """
+    Returns the matrix represented by M_ij in the paper arXiv:quant-ph/0407010
+    Is the matrix that converts the angles of k-fold uniformly controlled rotations
+    to the angles of the efficient gate decomposition.
+    :param k: number of control qubits
+    :return: the matrix M_ij
+    :rtype: numpy array
+    """
+    M = np.full((2**k, 2**k), 2**-k)
+    for i in range(2**k):
+        g_i = i ^ (i >> 1)  # Gray code for i
+        for j in range(2**k):
+            M[i, j] *= (-1)**(bin(j & g_i).count("1"))
+    return M
+
+
+def uniformly_controlled_cnot_control_positions(k):
+    """
+    Returns the list of positions of the controls of the CNOTs,
+    given that the target is the 0 qubit and higher order bits are in
+    consecutive integer order. The list gives the positions going
+    from left to right.
+    :param k: the number of control qubits
+    :return: the list of positions of the controls
+    :rtype: list
+    """
+    rotation_cnots = [1, 1]
+    for i in range(2, k+1):
+        # algorithm described is to replace the last control with a control to the new qubit
+        # and then repeat the sequence twice
+        rotation_cnots[-1] = i
+        rotation_cnots = rotation_cnots + rotation_cnots
+    return rotation_cnots
 
 
 def create_arbitrary_state(vector, offset=0):
@@ -44,31 +80,28 @@ def create_arbitrary_state(vector, offset=0):
     # the gates will be constructed in reverse order
     reversed_gates = []
 
-    # generate the matrix that will take the angles of k-fold uniformly controlled rotations
-    # and convert them to angles for the equivalent circuit of alternating uncontrolled rotations and CNOTs
-    M = np.full((N/2, N/2), 2**-(n-1))
-    for i in range(N/2):
-        g_i = i ^ (i >> 1)  # Gray code for i
-        for j in range(N/2):
-            M[i, j] *= (-1)**(bin(j & g_i).count("1"))
+    # matrix that converts angles of uniformly controlled rotation to angles of uncontrolled rotations
+    # at first, all qubits except for the 0 qubit act as controls
+    M = uniformly_controlled_rotation_matrix(n-1)
 
     # generate the positions of the controls for the CNOTs
-    rotation_cnots = [1, 1]
-    for i in range(2, n):
-        rotation_cnots[-1] = i
-        rotation_cnots = rotation_cnots + rotation_cnots
+    # at first, all qubits except for the 0 qubit act as controls
+    rotation_cnots = uniformly_controlled_cnot_control_positions(n-1)
 
     for step in range(n):
         z_thetas = []  # will hold the angles for controlled rotations in the phase unification step
         y_thetas = []  # will hold the angles for controlled rotations in the probabilities unification step
         for i in range(0, N, 2):
+            # find z rotation angles
             phi = phases[i]
             psi = phases[i+1]
             if i % 2**(step+1) == 0:  # due to repetition, only select angles are needed
                 z_thetas.append(phi - psi)
+            # update phases after applying such rotations
             kappa = (phi + psi)/2.
             phases[i], phases[i+1] = kappa, kappa
 
+            # find y rotation angles
             a = magnitudes[i]
             b = magnitudes[i+1]
             if i % 2**(step+1) == 0:  # due to repetition, only select angles are needed
@@ -76,6 +109,7 @@ def create_arbitrary_state(vector, offset=0):
                     y_thetas.append(0)
                 else:
                     y_thetas.append(2*np.arcsin((a-b)/(np.sqrt(2*(a**2 + b**2)))))
+            # update magnitudes after applying such rotations
             c = np.sqrt((a**2+b**2)/2.)
             magnitudes[i], magnitudes[i+1] = c, c
 
@@ -83,25 +117,27 @@ def create_arbitrary_state(vector, offset=0):
         converted_z_thetas = np.dot(M, z_thetas)
         converted_y_thetas = np.dot(M, y_thetas)
 
+        # just retain upper left square for the next iteration (one less control)
+        M = M[0:len(M)/2, 0:len(M)/2]*2
+
         # phase unification
         for j in range(len(converted_z_thetas)):
             if converted_z_thetas[j] != 0:
-                reversed_gates.append(RZ(-converted_z_thetas[j], qubits[0]))  # negative angle in conjugated/reversed circuit
+                # angle is negated in conjugated/reversed circuit
+                reversed_gates.append(RZ(-converted_z_thetas[j], qubits[0]))
             if step < n-1:
                 reversed_gates.append(CNOT(qubits[step + rotation_cnots[j]], qubits[0]))
 
         # probability unification
         for j in range(len(converted_y_thetas)):
             if converted_y_thetas[j] != 0:
-                reversed_gates.append(RY(-converted_y_thetas[j], qubits[0]))  # negative angle in conjugated/reversed circuit
+                # angle is negated in conjugated/reversed circuit
+                reversed_gates.append(RY(-converted_y_thetas[j], qubits[0]))
             if step < n-1:
                 reversed_gates.append(CNOT(qubits[step + rotation_cnots[j]], qubits[0]))
 
-        # just retain upper left square
-        M = M[0:len(M)/2, 0:len(M)/2]*2
-
+        # swaps are applied after all rotation steps except the last
         if step < n-1:
-            # swap
             reversed_gates.append(SWAP(qubits[0], qubits[step+1]))
 
             mask = 1 + (1 << (step + 1))
@@ -116,10 +152,9 @@ def create_arbitrary_state(vector, offset=0):
             rotation_cnots = rotation_cnots[:len(rotation_cnots) / 2]
             rotation_cnots[-1] -= 1
 
-    # Add Hadamard gates to unentangle to the 0 state
+    # Add Hadamard gates to remove superposition
     reversed_gates += map(H, qubits)
 
-    print vector, n
     # Correct the overall phase
     reversed_gates.append(PHASE(2*phases[0], qubits[0]))
     reversed_gates.append(RZ(-2*phases[0], qubits[0]))
