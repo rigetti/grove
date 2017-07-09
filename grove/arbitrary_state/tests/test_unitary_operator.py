dac8aeebba6dcb38705f82a6e5dc12db5b2f6676
joelin0 <joelin2014@gmail.com>
2017-07-08 19:25:24 -0700

    Feature/arbitrary state (#41)
    * Move from unitary_circuit branch and try to generate arbitrary states
    
    * Can generate arbitrary phase, fixed issues
    
    * Add documentation
    
    * Add ability to take in vectors with padded zeros
    
    * Add a case for when everything is zero except for last one
    
    * Move create arbitrary state method and console interface to own file
    
    * Marking tests as skipped for github build.
    
    * Make controlled gates without matrix square root
    
    * Revise arbitrary state generation to use many fewer gates
    
    * Add back in n_qubit_control to utils
    
    * Add check for if two consecutive coefficients are 0
    
    * Fix swap tracking issue
    
    * Fix issues to make arbitrary_state fully functional
    
    * Move old implementation of arbitrary state and utils into another branch
    
    * Add back imports which were removed for some reason
    
    * Add better documentation to the code
    
    * Add offset parameter
    
    * Add some modularity and additional documentation
    
    * Add tests for uniformlycontrolledrotationmatrix
    
    * Added tests for testing cnot position generation
    
    * Change spacing and placement of skip decorator
    
    * Rename test filename and add grove.arbitrary_state into setup.py
    
    * added harley's QR factorized state generation, and tests
    
    * Refactor unitary operator to remove program generation.
    
    * Clean up documentation for unitary_operator
    
    * Refactor some names and fix documentation in arbitrary_state.py
    
    * Simplify algorithm by only keeping track of unique phases/magnitudes
    
    * Refactor for adherence to PEP8
    
    * Added overview of arbitrary state generation to readthedocs
    
    * Make minor vector notation changes in method documentation
    
    * unit tests for unitary matrix
    
    * add docs for unitary
    
    * Made spacing changes for PEP8
    
    * Address wording issues in rst and get_cnot_control_positions docstring
    
    * Move the calculation of rotation parameters and updating phases and magnitudes to separate function
    
    * Move producing the reversed gates for each unification step to a separate function
    
    * Add documentation to get_unification_gates
    
    * Make get_unification_gates return a Program rather than list of gates
    
    * Added tests for get_rotation_parameters and get_reversed_unification_program functions
    
    * Remove print statement and change assertions to ValueErrors in unitary_operator

---
+++ grove/arbitrary_state/tests/test_unitary_operator.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+from grove.arbitrary_state.unitary_operator import unitary_operator, \
+    get_bits_needed, fix_norm_and_length
+
+
+def test_unitary():
+    length = 30
+    # make an arbitrary complex vector
+    v = np.random.uniform(-1, 1, length) \
+        + np.random.uniform(-1, 1, length) * 1j
+
+    # normalize and pad with zeros
+    v_norm = v / np.linalg.norm(v)
+    num_bits = get_bits_needed(length)
+    while len(v_norm) < 2 ** num_bits:
+        v_norm = np.append(v_norm, 0)
+
+    # generate unitary operator
+    U = unitary_operator(v_norm)
+
+    # make sure U|0> = |v>
+    zero_state = np.zeros(len(U))
+    zero_state[0] = 1
+    assert np.allclose(U.dot(zero_state), v_norm)
+
+
+def test_fix_norm_and_length():
+    length = 30
+    # make an arbitrary complex vector
+    v = np.random.uniform(-1, 1, length) \
+        + np.random.uniform(-1, 1, length) * 1j
+
+    new_v = fix_norm_and_length(v)
+    assert np.allclose([np.linalg.norm(new_v)], [1])
+    assert len(new_v) == 2 ** get_bits_needed(len(v))
+
+
+def test_get_bits_needed():
+    for i, j in zip([1, 2, 5, 8, 9, 14, 27], [1, 1, 3, 3, 4, 4, 5]):
+        assert get_bits_needed(i) == j
