gradients output correct nonzero results, now need to group by branch

grove/pyqaoa/analytical_gradient/minimal_ag.py
@@ -52,8 +52,6 @@ import pyquil.quil as pq
 import pyquil.api as api
 from pyquil.gates import *
 
-import referenceqvm
-
 
 if __name__ == "__main__":
     """Constructs a minimal ag implementation
@@ -108,71 +106,52 @@ if __name__ == "__main__":
     cost_gradient_program.inst(S(2))
     cost_gradient_program.inst(H(2))
 
-    right_program = cost_gradient_program + cost_hamiltonian_program
-    left_program = cost_gradient_program
-    #print(right_program)
-    #print(left_program)
-    right_amplitudes = qvm.wavefunction(right_program)[0].amplitudes
-    left_amplitudes = qvm.wavefunction(left_program)[0].amplitudes
-    #print(right_amplitudes)
-    #print(left_amplitudes)
-    #print(np.dot(np.conj(left_amplitudes),right_amplitudes))
+    print(cost_gradient_program)
 
     expectation = qvm.expectation(cost_gradient_program,
             operator_programs=[cost_hamiltonian_program])[0]
-    #print(expectation)
+    print(expectation)
 
-    ########################################
-    #Constructs the driver gradient program#
-    ########################################
-    #driver_gradient_program = pq.Program()
-    driver_gradient_program = pq.Program()
+    #########################################
+    #Constructs the driver gradient programs#
+    #########################################
+    driver_gradient_program_0 = pq.Program()
 
     #Reference State Prep
-    driver_gradient_program.inst(H(0))
-    driver_gradient_program.inst(H(1))
+    driver_gradient_program_0.inst(H(0))
+    driver_gradient_program_0.inst(H(1))
     #Ancilla Prep
-    driver_gradient_program.inst(H(2))
+    driver_gradient_program_0.inst(H(2))
 
     #The exponentiated Z(0)Z(1) term
-    driver_gradient_program.inst(CNOT(0,1))
-    driver_gradient_program.inst(RZ(gamma)(1))
-    driver_gradient_program.inst(CNOT(0,1))
+    driver_gradient_program_0.inst(CNOT(0,1))
+    driver_gradient_program_0.inst(RZ(gamma)(1))
+    driver_gradient_program_0.inst(CNOT(0,1))
 
     #X(0) and X(1) gates become CNOT(2,0) and CNOT(2,1) gates respectively
-    driver_gradient_program.inst(CNOT(2,0))
-    #driver_gradient_program.inst(CNOT(2,1))
+    driver_gradient_program_0.inst(CNOT(2,0))
+    #driver_gradient_program_0.inst(CNOT(2,1))
 
     #The exponentiated X(0) Term
-    driver_gradient_program.inst(H(0))
-    driver_gradient_program.inst(RZ(beta)(0))
-    driver_gradient_program.inst(H(0))
+    driver_gradient_program_0.inst(H(0))
+    driver_gradient_program_0.inst(RZ(beta)(0))
+    driver_gradient_program_0.inst(H(0))
 
     #The exponentiated X(1) Term
-    driver_gradient_program.inst(H(1))
-    driver_gradient_program.inst(RZ(beta)(1))
-    driver_gradient_program.inst(H(1))
+    driver_gradient_program_0.inst(H(1))
+    driver_gradient_program_0.inst(RZ(beta)(1))
+    driver_gradient_program_0.inst(H(1))
 
     #The final entanling ancilla operations
-    driver_gradient_program.inst(S(2))
-    driver_gradient_program.inst(H(2))
+    driver_gradient_program_0.inst(S(2))
+    driver_gradient_program_0.inst(H(2))
 
-    #print(driver_gradient_program)
-    #print(cost_hamiltonian_program)
-
-    right_program = driver_gradient_program + cost_hamiltonian_program
-    left_program = driver_gradient_program
-    #print(right_program)
-    #print(left_program)
-    right_amplitudes = qvm.wavefunction(right_program)[0].amplitudes
-    left_amplitudes = qvm.wavefunction(left_program)[0].amplitudes
-    #print(right_amplitudes)
-    #print(left_amplitudes)
-    print(np.dot(np.conj(left_amplitudes), right_amplitudes))
+    #print(driver_gradient_program_0)
+    #print(cost_hamiltonian_program_0)
 
 
-    print(np.cos(2*beta)*np.sin(gamma))
+    #print(np.cos(2*beta)*np.sin(gamma))
 
-    #expectation = qvm.expectation(driver_gradient_program,
-    #        operator_programs=[cost_hamiltonian_program])[0]
+    #expectation = qvm.expectation(driver_gradient_program_0,
+    #        operator_program_0s=[cost_hamiltonian_program])[0]
     #print(expectation)