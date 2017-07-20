97c324912fabb82c84e5a7320f8f8936252b804a
richardzhu <richard.zhu@rigetti.com>
2017-07-20 13:02:02 -0700

    Removed pybond dependency in BK test

--- grove/fermion_transforms/tests/test_bravyi_kitaev.py
+++ grove/fermion_transforms/tests/test_bravyi_kitaev.py
@@ -1,24 +1,16 @@
 from grove.fermion_transforms.bktransform import BKTransform
-from pybond.bk_hamiltonian import BKHamiltonian
 
-def test_bkh():
-    # test rel. BK_Hamiltonian
+
+def test_bravyi_kitaev():
+    # test drawn from ProjectQ/FermiLib/BKTransform
     n_qubits = 16
     bkt = BKTransform(n_qubits)
     x = bkt.kill(9)
     y = bkt.create(9)
 
-    bkh = BKHamiltonian()
-    bkh.n_qubits = 16
-    xp = bkh.fermi_to_pauli(1.0, [9], [1])  # kill
-    yp = bkh.fermi_to_pauli(1.0, [9], [-1])  # create
-
-    assert str(x) == str(xp)
-    assert str(y) == str(yp)
+    # proper answer obtained from pybond.bkhamiltonian
     assert str(x) == '0.5j*Z7*Y9*X11*X15 + 0.5*Z7*Z8*X9*X11*X15'
     assert str(y) == '-0.5j*Z7*Y9*X11*X15 + 0.5*Z7*Z8*X9*X11*X15'
 
-    # test rel. BK_Hamiltonian
-
-
-    # test rel. power-of-2 Bravyi-Kitaev
+if __name__ == '__main__':
+    test_bravyi_kitaev()
