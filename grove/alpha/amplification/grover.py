1e430157b27fdb796281ee5b2d2ef4826d0cba30
Anthony Polloreno <anthony@rigetti.com>
2017-10-07 15:13:40 -0700

    removing print statement

--- grove/alpha/amplification/grover.py
+++ grove/alpha/amplification/grover.py
@@ -17,7 +17,6 @@ def grover(bitstring_map):
     oracle_unitary = compute_grover_oracle_matrix(bitstring_map)
     oracle = pq.Program()
     oracle_name = "ORACLE"
-    print oracle_unitary
     oracle.defgate(oracle_name, oracle_unitary)
     number_of_qubits = oracle_unitary.shape[0]
     qubits = [oracle.alloc() for _ in range(number_of_qubits / 2)]
