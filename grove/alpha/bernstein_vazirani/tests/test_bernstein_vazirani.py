514c0b051a33eb6b7a0cdcf7637f6c380a0e141d
Johannes Otterbach <johannesotterbach@gmail.com>
2017-10-10 10:53:51 -0700

    Test coverage using mock

--- grove/alpha/bernstein_vazirani/tests/test_bernstein_vazirani.py
+++ grove/alpha/bernstein_vazirani/tests/test_bernstein_vazirani.py
@@ -3,8 +3,47 @@ import pyquil.api as api
 import pyquil.quil as pq
 import pytest
 from pyquil.gates import X
+from mock import patch
 
-from grove.alpha.bernstein_vazirani.bernstein_vazirani import bernstein_vazirani, oracle_function
+from pyquil.api import SyncConnection
+from pyquil.quil import Program
+
+from grove.alpha.bernstein_vazirani.bernstein_vazirani import bernstein_vazirani, oracle_function, run_bernstein_vazirani
+
+def _create_expected_prog():
+    expected_prog = Program()
+    expected_prog.inst("X 3")
+    expected_prog.inst("H 3")
+    expected_prog.inst("H 0")
+    expected_prog.inst("H 1")
+    expected_prog.inst("H 2")
+    expected_prog.inst("X 3")
+    expected_prog.inst("CNOT 2 3")
+    expected_prog.inst("CNOT 1 3")
+    expected_prog.inst("H 0")
+    expected_prog.inst("H 1")
+    expected_prog.inst("H 2")
+    return expected_prog
+
+def test_bernstein_vazirani():
+    orc = oracle_function([1, 1, 0], 1, [0, 1, 2], 3)
+    bv_prog = bernstein_vazirani(orc, [0, 1, 2], 3)
+
+    with patch("pyquil.api.SyncConnection") as qvm:
+        # Need to mock multiple returns as an iterable
+        qvm.run_and_measure.side_effect = [
+            ([0, 1, 1], ),
+            ([1], )
+        ]
+
+
+    # qvm = SyncConnection("http://127.0.0.1:5000")
+
+    bv_a, bv_b, bv_program = run_bernstein_vazirani(qvm, orc, [0, 1, 2], 3)
+
+    assert bv_a == [1, 1, 0]
+    assert bv_b == 1
+    assert bv_program.__str__() == _create_expected_prog().__str__()
 
 
 @pytest.mark.skip(reason="Must add support for Forest connections in testing")
