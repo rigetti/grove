aac855cb0339f87e1046811b9097858201cfe841
kmckiern <kmckiern@stanford.edu>
2017-11-13 15:06:44 -0800

    isolate helper functions

--- grove/alpha/jordan_gradient/gradient_helper.py
+++ grove/alpha/jordan_gradient/gradient_helper.py
@@ -1,5 +1,4 @@
 import numpy as np
-from jordan_gradient import gradient_estimator
 
 def real_to_binary(number, precision=16):
     """ Convert real decimal to precision-bit binary fraction
@@ -54,35 +53,3 @@ def stats_to_bf(stats):
     
     return bf
 
-def gradient_error(f_h, precision=5, n_measurements=100):
-    """ Computes error of gradient estimates for an input perturbation value
-    
-    :param np.array/float f_h: Value of f at perturbation h.
-    :param int n_measurements: Number of times to run the gradient program.
-    :return float error: Error of gradient estimate.
-    """
-
-    if isinstance(f_h, float):
-        d = 1 # f_h = np.array(f_h)
-    else:
-        d = f_h.ndim
-
-    # enumerate qubit register
-    N_qi = d * precision
-    input_qubits = list(range(N_qi))
-    ancilla_qubits = [N_qi]
-
-    # build program and run n_measurements times
-    p_g = gradient_estimator(f_h, input_qubits, ancilla_qubits, precision)
-
-    from pyquil.api import SyncConnection
-    qvm = SyncConnection()
-    measurements = np.array(qvm.run(p_g, input_qubits, n_measurements))
-
-    # summarize measurements and compute error
-    stats = measurements.sum(axis=0) / len(measurements)
-    bf_estimate = stats_to_bf(stats)
-    deci_estimate = binary_to_real(bf_estimate)
-    error = f_h - deci_estimate
-    
-    return error
