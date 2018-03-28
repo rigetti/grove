38a18543bb0788fd8d0aa624380d727e2d12cdc0
Mark Fingerhuth <markfingerhuth@protonmail.com>
2018-03-28 15:46:06 -0400

    renamed ising() to ising_qaoa() to make clear that it is a QAOA wrapper

--- grove/ising/ising_qaoa.py
+++ grove/ising/ising_qaoa.py
@@ -51,7 +51,7 @@ def ising_trans(x):
         return 1
 
 
-def ising(h, J, num_steps=0, verbose=True, rand_seed=None, connection=None, samples=None,
+def ising_qaoa(h, J, num_steps=0, verbose=True, rand_seed=None, connection=None, samples=None,
           initial_beta=None, initial_gamma=None, minimizer_kwargs=None,
           vqe_option=None):
     """
