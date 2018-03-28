23221f10f00a2448b884c00a7e6d708be72d4c34
Mark Fingerhuth <markfingerhuth@protonmail.com>
2018-03-28 15:58:19 -0400

    fixed existing tests for ising_qaoa()

--- grove/tests/ising/test_ising.py
+++ grove/tests/ising/test_ising.py
@@ -1,4 +1,4 @@
-from grove.ising.ising_qaoa import ising
+from grove.ising.ising_qaoa import ising_qaoa
 from grove.ising.ising_qaoa import energy_value
 import numpy as np
 from mock import patch
@@ -6,7 +6,7 @@ from mock import patch
 
 def test_energy_value():
     J = {(0, 1): 2.3}
-    h = [-2.4, 5.2]
+    h = {0: -2.4, 1: 5.2}
     sol = [1, -1]
     ener_ising = energy_value(h, J, sol)
 
@@ -20,9 +20,9 @@ def test_ising_mock():
         cxn.expectation.return_value = [-0.4893891813015294, 0.8876822987380573, -0.4893891813015292, -0.9333372094534063, -0.9859245403423198, 0.9333372094534065]
 
     J = {(0, 1): -2, (2, 3): 3}
-    h = [1, 1, -1, 1]
+    h = {0: 1, 1: 1, 2: -1, 3: 1}
     p = 1
-    most_freq_string_ising, energy_ising, circuit = ising(h, J, num_steps=p, vqe_option=None, connection=cxn)
+    most_freq_string_ising, energy_ising, circuit = ising_qaoa(h, J, num_steps=p, vqe_option=None, connection=cxn)
 
     assert most_freq_string_ising == [-1, -1, 1, -1]
     assert energy_ising == -9
