Estimating sets of pauli sums to fixed variance
Estimates the expected value of a linear combination of pauli terms to a
fixed variance on the estimator.  The variance of the pauli sum is
estimated by sample variance.  Example notebooks will be added later.

grove/measurements/estimation.py
@@ -2,9 +2,13 @@
 Utilities for estimating expected values of Pauli terms given pyquil programs
 """
 import numpy as np
-from pyquil.paulis import PauliSum, PauliTerm
+from pyquil.paulis import PauliSum, PauliTerm, commuting_sets, sI
 from pyquil.quil import Program
 from pyquil.gates import RY, RX
+
+
+class CommutationError(Exception):
+    pass
 
 
 def remove_imaginary_terms(pauli_sums):
@@ -19,7 +23,7 @@ def remove_imaginary_terms(pauli_sums):
     new_term = sI(0) * 0.0
     for term in pauli_sums:
         if (not np.isclose(term.coefficient.real, 0.0) and
-           np.isclose(term.coefficient.imag, 0.0)):
+                np.isclose(term.coefficient.imag, 0.0)):
             new_term += term
 
     return new_term
@@ -47,72 +51,111 @@ def get_rotation_program(pauli_term):
     return meas_basis_change
 
 
-def estimate_psum(operator, program, variance_bound,
-                  quantum_resource, grouping=None):
+def get_parity(pauli_terms, bitstring_results):
     """
-    Compute the expected value of an operator given an variance bound
+    Calculate the parity at each of the marked qubits in pauli_terms
 
-    :param operator: PauliSum or PauliTerm to sample
+    Each list of bits in the bitstring_results is the maximum weight pauli term
+    in pauli_terms.  The bits in each element of bitstring_results correspond
+    to a numerical ordering of the maximal weight element of pauli_terms.
+
+    An example:
+
+    A maximum weight pauli term is Z1Z5Z6.
+
+    each element of bitstring_results is something in
+    :math:`\{0, 1\}^{\otimes 3}` where the first bit corresponds to the measured
+    value of qubit 1, the second bit corresponds to the measured value of qubit
+    5, and the third bit corresponds to the measured value of qubit 6.
+
+    :param pauli_terms:
+    :param bitstring_results:
+    :return:
+    """
+    max_weight_pauli_index = np.argmax(
+        [len(term.get_qubits()) for term in pauli_terms])
+    active_qubit_indices = sorted(
+        pauli_terms[max_weight_pauli_index]._ops.keys())
+    index_mapper = dict(zip(active_qubit_indices,
+                            range(len(active_qubit_indices))))
+
+    results = np.zeros((len(pauli_terms), len(bitstring_results)))
+
+    # convert to array so we can fancy index into it later.
+    # list() is needed to cast because we don't really want a map object
+    bitstring_results = list(map(np.array, bitstring_results))
+    for row_idx, term in enumerate(pauli_terms):
+        memory_index = np.array(list(map(lambda x: index_mapper[x],
+                                         sorted(term.get_qubits()))))
+
+        results[row_idx, :] = [-2 * (sum(x[memory_index]) % 2) + \
+                               1 for x in bitstring_results]
+    return results
+
+
+def estimate_pauli_set(pauli_term_set, basis_transform_dict, program,
+                       variance_bound, quantum_resource):
+    """
+    Estimate the mean of a sum of pauli terms to set variance
+
+    The sample variance is calculated by
+
+    .. math::
+        \begin{align}
+        \mathrm{Var}[\hat{\langle H \rangle}] = \sum_{i, j}h_{i}h_{j}
+        \mathrm{Cov}(\hat{\langle P_{i} \rangle}, \hat{\langle P_{j} \rangle})
+        \end{align}
+
+    :param pauli_term_set: list of pauli terms to measure simultaneously
+    :param basis_transform_dict: basis transform dictionary where the key is
+                                 the qubit index and the value is the basis to
+                                 rotate into. Valid basis is [I, X, Y, Z].
     :param program: program generating a state to sample from
     :param variance_bound:  Bound on the variance of the estimator for the
                             PauliSum. Remember this is the SQUARE of the
                             standard error!
-    :param assignment_prob_map: dictionary mapping qubit terms to assignment
-                                error matrix
+
     :param quantum_resource: quantum abstract machine object
-    :param grouping: (Default None) function for grouping the pauli terms of the
-                     operator.
-    :return: estimated expected alue
+    :return: estimated expected value, covariance matrix, variance of the
+             estimator, and the number of shots taken
     """
-    if not isinstance(operator, (PauliTerm, PauliSum)):
-        raise TypeError("I only accept PauliTerm or PauliSum")
+    if not isinstance(pauli_term_set, list):
+        raise TypeError("pauli_term_set needs to be a list")
 
-    if isinstance(operator, PauliTerm):
-        operator = PauliSum([operator])
+    # check if each term commutes with everything
+    if len(commuting_sets(sum(pauli_term_set))) != 1:
+        raise CommutationError("Not all terms commute in the expected way")
 
-    # # group into z-basis sets
-    # diagonal_basis_grouping = commuting_sets_by_zbasis(operator)
-    # for key, value in diagonal_basis_grouping.items():
-    #     print(key, sum(value))
-    if grouping is None:
-        # use default grouping which is the trival commuting terms
+    pauli_for_rotations = PauliTerm.from_list(
+        [(value, key) for key, value in basis_transform_dict.items()])
+    post_rotations = get_rotation_program(pauli_for_rotations)
 
-    else:
-        num_terms = len(operator)
-        variance_bound_per_term = variance_bound / num_terms
-        operator_mean_estimator = 0.0
-        operator_mean_variance = 0.0
-        for term in operator:
-            if term.id() != "":
-                post_rotations = get_rotation_program(term)
+    coeff_vec = np.array(
+        list(map(lambda x: x.coefficient, pauli_term_set))).reshape((-1, 1))
 
-                number_of_shots = int(np.ceil(abs(term.coefficient)**2 /
-                                          variance_bound_per_term))
-                num_bins = number_of_shots // 10000
-                overflow_bin = number_of_shots % 10000
-                results = []
-                for _ in range(num_bins):
-                    tresults = quantum_resource.run_and_measure(
-                        program + post_rotations, list(term.get_qubits()),
-                        trials=10000)
-                    results.extend(tresults)
+    results = None
+    sample_variance = np.infty
+    while sample_variance > variance_bound:
+        # note: bit string values are returned according to their listed order
+        # in run_and_measure.  Therefore, sort beforehand to keep alpha numeric
+        tresults = quantum_resource.run_and_measure(
+            program + post_rotations,
+            sorted(list(basis_transform_dict.keys())),
+            trials=10000)
 
-                if overflow_bin > 0:
-                    tresults = quantum_resource.run_and_measure(
-                        program + post_rotations, list(term.get_qubits()),
-                        trials=overflow_bin)
-                    results.extend(tresults)
+        parity_results = get_parity(pauli_term_set, tresults)
 
-                ########################################################
-                # observed values and variance given the sample variance
-                ########################################################
-                obs_val = np.array([-2 * (sum(x) % 2) + 1 for x in results])
-                mean_val = np.mean(obs_val)
-                sample_var = sum((obs_val - mean_val) ** 2) / (len(obs_val) - 1)
-                operator_mean_estimator += term.coefficient * mean_val
-                operator_mean_variance += abs(term.coefficient)**2 * sample_var / number_of_shots
+        # Note: easy improvement would be to update mean and variance on the fly
+        # instead of storing all these results.
+        if results is None:
+            results = parity_results
+        else:
+            results = np.hstack((results, parity_results))
 
-            else:
-                operator_mean_estimator += term.coefficient
+        # calculate the expected values....
+        covariance_mat = np.cov(results)
+        sample_variance = coeff_vec.T.dot(covariance_mat).dot(coeff_vec) / \
+                          results.shape[1]
 
-        return operator_mean_estimator, operator_mean_variance
+    return coeff_vec.T.dot(np.mean(results, axis=1)), covariance_mat, \
+           sample_variance, results.shape[1]