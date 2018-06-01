decc5a3ecb4f95bfb0c0d745d028aa43d759f779
kmckiern <keri@rigetti.com>
2018-06-01 10:30:35 -0700

    more pythonic

--- grove/measurements/estimation.py
+++ grove/measurements/estimation.py
@@ -147,9 +147,8 @@ def estimate_pauli_sum(pauli_terms, basis_transform_dict, program,
     pauli_for_rotations = PauliTerm.from_list(
         [(value, key) for key, value in basis_transform_dict.items()])
 
-    post_rotations = get_rotation_program(pauli_for_rotations)
+    program += get_rotation_program(pauli_for_rotations)
 
-    program = program + post_rotations
     qubits = sorted(list(basis_transform_dict.keys()))
     for qubit in qubits:
         program.inst(MEASURE(qubit, qubit))
