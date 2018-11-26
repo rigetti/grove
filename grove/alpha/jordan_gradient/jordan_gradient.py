44befc210cacf7e3aee14ffe361d7a1391af6a38
Amy Brown <44273795+amyfbrown@users.noreply.github.com>
2018-11-26 15:11:35 -0800

    Replace QVMConnection in jordan_gradient

--- grove/alpha/jordan_gradient/jordan_gradient.py
+++ grove/alpha/jordan_gradient/jordan_gradient.py
@@ -1,20 +1,19 @@
-from __future__ import division
-
 import numpy as np
+from pyquil import Program
+from pyquil.api import QuantumComputer, get_qc
+
+from grove.alpha.jordan_gradient.gradient_utils import (binary_float_to_decimal_float,
+                                                        measurements_to_bf)
 from grove.alpha.phaseestimation.phase_estimation import phase_estimation
 
-from grove.alpha.jordan_gradient.gradient_utils import binary_to_real, \
-    measurements_to_bf
 
+def gradient_program(f_h: float, precision: int) -> Program:
+    """
+    Gradient estimation via Jordan's algorithm (10.1103/PhysRevLett.95.050501).
 
-def gradient_program(f_h, precision):
-    """Gradient estimation via Jordan's algorithm
-    10.1103/PhysRevLett.95.050501
-
-    :param float f_h: Oracle output at perturbation h.
-    :param list ancilla_qubits: Qubits of output register.
+    :param f_h: Oracle output at perturbation h.
+    :param precision: Bit precision of gradient.
     :return: Quil program to estimate gradient of f.
-    :rtype: Program
     """
 
     # encode oracle values into phase
@@ -26,17 +25,19 @@ def gradient_program(f_h, precision):
     return p_gradient
 
 
-def estimate_gradient(f_h, precision, gradient_max=1, n_measurements=50, 
-                      cxn=None):
-    """Estimate the gradient using function evaluation at perturbation, h
+def estimate_gradient(f_h: float, precision: int,
+                      gradient_max: int = 1,
+                      n_measurements: int = 50,
+                      qc: QuantumComputer = None) -> float:
+    """
+    Estimate the gradient using function evaluation at perturbation, h.
 
-    :param float f_h: Oracle output at perturbation h.
-    :param int precision: Bit precision of gradient.
-    :param int gradient_max: OOM estimate of largest gradient value
-    :param int n_measurements: Number of times to measure system.
-    :param Connection cxn: Connection to the QPU or QVM.
+    :param f_h: Oracle output at perturbation h.
+    :param precision: Bit precision of gradient.
+    :param gradient_max: OOM estimate of largest gradient value
+    :param n_measurements: Number of times to measure system.
+    :param qc: The QuantumComputer object.
     :return: Decimal estimate of gradient.
-    :rtype: float
     """
 
     # scale f_h by range of values gradient can take on
@@ -47,16 +48,17 @@ def estimate_gradient(f_h, precision, gradient_max=1, n_measurements=50,
     p_gradient = gradient_program(f_h, precision)
 
     # run gradient program
-    if cxn is None:
-        from pyquil.api import QVMConnection
-        cxn = QVMConnection()
-    measured_qubits = list(range(precision + 1))
-    measurements = cxn.run(p_gradient, measured_qubits, n_measurements)
+    if qc is None:
+        qc = get_qc("9q-square-qvm")
+
+    p_gradient.wrap_in_numshots_loop(n_measurements)
+    executable = qc.compiler.native_quil_to_executable(p_gradient)
+    measurements = qc.run(executable)
 
     # summarize measurements
     bf_estimate = perturbation_sign * measurements_to_bf(measurements)
     bf_explicit = '{0:.16f}'.format(bf_estimate)
-    deci_estimate = binary_to_real(bf_explicit)
+    deci_estimate = binary_float_to_decimal_float(bf_explicit)
     
     # rescale gradient
     deci_estimate *= gradient_max
